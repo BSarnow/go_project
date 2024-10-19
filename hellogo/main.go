f2f0e7558b8fe0df9b8d77d703765a0a568d55b7
Sarnweis <BenjaminSarnow@web.de>
2024-10-19 17:38:54 +0200

    mystrings added

---
+++ hellogo/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/BSarnow/mystrings"
+)
+
+func main() {
+	fmt.Println(mystrings.Reverse("hello world"))
+}package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
---
+++ mystrings/go.mod
@@ -0,0 +1,3 @@
+module github.com/BSarnow/mystrings
+
+go 1.23.2
